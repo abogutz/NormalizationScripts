fe82b14d5020f13a8ded5cacb58548392a12294b
abogutz <37719032+abogutz@users.noreply.github.com>
2023-05-16 16:11:21 -0700

    Update spikeMapCalc.sh

--- spikeMapCalc.sh
+++ spikeMapCalc.sh
@@ -19,27 +19,36 @@ module load bwa/0.7.17
 
 
 RUN_THREAD=20
-SCRATCH="$HOME/scratch/"
+SCRATCH="$HOME/scratch/$SLURM_JOB_ID/"
+mkdir $SCRATCH
 
 READ_LEN=66
 SLIDING_WIN=5
 
 REF=$1
+SIZES=${REF//.fa/.sizes}
 BED=$2
 SPIKE=$3
+
+REF_NAME=$(basename $REF)
+REF_NAME=${REF_NAME//.fa/}
+SPIKE_NAME=$(basename $SPIKE)
+SPIKE_NAME=${SPIKE_NAME//.fa/}
+echo "Testing "$REF_NAME" and "$SPIKE_NAME"..."
 
 REF_BED="$SCRATCH/Ref.bed"
 REF_FASTA="$SCRATCH/Ref.fa"
 REF_FASTQ="$SCRATCH/Ref_R"$READ_LEN"-w"$SLIDING_WIN".fq"
 REF_FASTQ_GZ="$REF_FASTQ.gz"
 SAM="$SCRATCH/temp.sam"
-REF_BAM=${REF//.fa/}"_R"$READ_LEN"-w"$SLIDING_WIN".bam"
-REF_TXT=${REF//.fa/}"_R"$READ_LEN"-w"$SLIDING_WIN".txt"
+REF_BAM=$REF_NAME"_R"$READ_LEN"-w"$SLIDING_WIN".bam"
+REF_TXT=$REF_NAME"_R"$READ_LEN"-w"$SLIDING_WIN".txt"
 CAT="$SCRATCH/${REF//.fa/}"+"${SPIKE//.fa/}.fa"
-CAT_BAM=${REF//.fa/}"+"${SPIKE//.fa/}"_R"$READ_LEN"-w"$SLIDING_WIN".bam"
-CAT_TXT=${REF//.fa/}"+"${SPIKE//.fa/}"_R"$READ_LEN"-w"$SLIDING_WIN".txt"
+CAT_BAM=$REF_NAME"+"$SPIKE_NAME"_R"$READ_LEN"-w"$SLIDING_WIN".bam"
+CAT_TXT=$REF_NAME"+"$SPIKE_NAME"_R"$READ_LEN"-w"$SLIDING_WIN".txt"
 
 if [[ ! -f $REF_FASTQ_GZ ]]; then
+	echo "Creating windowed fastq..."
 	bedtools makewindows -w $READ_LEN -s $SLIDING_WIN -b $BED > $REF_BED
 	bedtools getfasta -fi $REF -fo $REF_FASTA -bed $REF_BED
 	rm $REF_BED
@@ -49,7 +58,7 @@ if [[ ! -f $REF_FASTQ_GZ ]]; then
 fi
 
 if [[ ! -f $REF_TXT ]]; then
-	bwa index $REF
+#	bwa index $REF
 	bwa mem -t $RUN_THREAD $REF $REF_FASTQ_GZ > $SAM
 	samtools view -bhS -@ $RUN_THREAD $SAM > $REF_BAM
 
@@ -58,10 +67,10 @@ if [[ ! -f $REF_TXT ]]; then
 	 } END {for(x=0; x<256; x++){print x, a[x]}}' > $REF_TXT
 fi
 
-if [[ ! -f $CAT ]]; then
-	CHR_PREFIX=">${REF//.fa/}_"
+if [[ ! -f $CAT ]]; then # Create concatenated genome
+	CHR_PREFIX=">$REF_NAME_"
 	cat $REF | sed "s/>/$CHR_PREFIX/g" > temp
-	CHR_PREFIX=">${SPIKE//.fa/}_"
+	CHR_PREFIX=">$SPIKE_NAME_"
 	cat $SPIKE | sed "s/>/$CHR_PREFIX/g" > temp2
 	cat temp temp2 > $CAT
 	rm temp temp2
@@ -80,4 +89,14 @@ samtools view $CAT_BAM | cut -f 5 | awk 'BEGIN {
 	a[$1]++;
 } END {for(x=0; x<256; x++){print x, a[x]}}' > $CAT_TXT
 
+for FILE in $REF_BAM $CAT_BAM
+do
+	samtools view $FILE | awk -v win=$SLIDING_WIN 'OFS="\t"{
+		split($1, a, /[:,]/);
+		print a[1], a[2], a[2]+win-1, $5/60;
+	}' > ${FILE//.bam/.bedgraph}
+	bedGraphToBigWig ${FILE//.bam/.bedgraph} $SIZES ${FILE//.bam/_mappability.bw}
+done
+
+rm -r $SCRATCH
 
